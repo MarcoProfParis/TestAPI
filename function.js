39034e189c0c247bef1f315483e8862611531f98
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-04-10 19:40:43 +0200

    x

--- function.js
+++ function.js
@@ -1,30 +1,25 @@
 window.function = async function(key1, jsonstring) {
     if (jsonstring.value === undefined || jsonstring.value === '') return "En attente json...";
     let json = JSON.parse(jsonstring.value);
+    let apikey = key1.value;
+    console.log("apikey:", apikey);
     const apiUrl = json.url;
-    const requestOptions = { mode: 'no-cors' };
+    const headers = {"X-CMC_PRO_API_KEY":"${apikey}","Accept":"application/json"};
+    console.log("headers:", headers);
+    const requestOptions = { 
+        method:'GET',
+        headers: headers
+ };
     console.log("apiUrl:", apiUrl);
     console.log("requestOptions:", requestOptions);
     try {
-        let delivery = await api(apiUrl); // await here
-        console.log("delivery:", delivery);
-        return delivery;
+        let response = await fetch(apiUrl,requestOptions); // await here
+        console.log("response:", response);
+        const text = await response.text();
+        console.log("text:", text);
+        return text;
     } catch (error) {
         console.error("Error:", error);
         return "Error fetching data"; // Handle error accordingly
     }
 };
-
-async function api(apiUrl,requestOptions){ // async keyword here
-    try {
-        const response = await fetch(apiUrl,requestOptions);
-        console.log("response:", response);
-        const text = await response.text();
-        console.log("text:", text);
-        let data = JSON.parse(text);
-        return text;
-    } catch (error) {
-        console.error("API Error:", error);
-        throw error; // Rethrow the error to handle it in the calling function
-    }
-}
